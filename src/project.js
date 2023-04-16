7aa9c17da1cb1dc40ee75909a8fbf20dd8d55132
Mike <nhj25252@gmail.com>
2023-04-16 11:42:59 +0900

    set function to find project to insert task

--- src/project.js
+++ src/project.js
@@ -1,3 +1,5 @@
+import { taskArray } from "./task";
+
 export class Project {
   constructor(title, dueDate) {
     this.title = title;
@@ -30,4 +32,13 @@ export function deleteProject(ID) {
   const indexToRemove = projectsArr.findIndex((proj) => proj.id === ID);
   projectsArr.splice(indexToRemove, 1);
   localStorage.setItem("storageProjectsArr", JSON.stringify(projectsArr));
+}
+
+// function to take in task
+// maybe import taskArray from task.js,
+// replace task property of each project with the taskArray?
+
+function findProjectToInsertTask(ID) {
+  const projectToInsertTask = projectsArr.find((proj) => proj.id === ID);
+  return projectToInsertTask;
 }
