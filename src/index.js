5bb27572c18f0749f77f212a3d15a514d295516e
Mike <nhj25252@gmail.com>
2023-04-27 19:32:43 +0900

    ui: delete add svg

--- src/index.js
+++ src/index.js
@@ -8,29 +8,12 @@ const projectTitle = document.querySelector(".project-title");
 
 rerenderProjectArr();
 
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (projectTitle.value !== "") {
+projectTitle.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+
     addProject();
     rerenderProjectArr();
     projectTitle.value = "";
   }
 });
-
-projectTitle.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    addBtn.click();
-  }
-});
-
-// const taskAddBtn = document.querySelector(".task-add-btn");
-// const taskTitle = document.querySelector(".task-title");
-
-// rerenderTaskArr();
-
-// taskAddBtn.addEventListener("click", () => {
-//   addTask();
-//   rerenderTaskArr();
-//   taskTitle.value = "";
-// });
